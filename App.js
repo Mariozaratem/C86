9950ec6f1106be1511ae42289275bc5eba9bbc5e
Mariozaratem <114197423+Mariozaratem@users.noreply.github.com>
2023-08-12 07:44:47 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./Screens/Home"; // Ajusta la ruta de importación según tu estructura de archivos
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }} // Oculta la barra de navegación
+        />
+        {/* Agrega más pantallas aquí si es necesario */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
